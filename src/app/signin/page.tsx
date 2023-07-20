7a249d17fb02834bc80d8caf5c144d8308f8b22a
Roger Torres <roger.torres@andela.com>
2023-07-20 18:14:32 -0400

    fix: getUrl

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -5,6 +5,18 @@ import { Auth } from "@supabase/auth-ui-react"
 import { ThemeSupa } from "@supabase/auth-ui-shared"
 // import { signIn } from "next-auth/react"
 
+export const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/"
+  // Make sure to include `https://` when not localhost.
+  url = url.includes("http") ? url : `https://${url}`
+  // Make sure to including trailing `/`.
+  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`
+  return url
+}
+
 export default function SignIn() {
   // TODO: Next-Auth signin ui
   // signIn("google", { callbackUrl: "/dashboard" })
@@ -16,7 +28,7 @@ export default function SignIn() {
         <Auth
           supabaseClient={supabase}
           providers={["google"]}
-          redirectTo={`/auth/callback`}
+          redirectTo={`${getURL()}/auth/callback`}
           magicLink={true}
           appearance={{
             theme: ThemeSupa,
