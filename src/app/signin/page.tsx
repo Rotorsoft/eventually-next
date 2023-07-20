976e933d3a0f87e31569f4b86391e536eede1e13
Roger Torres <roger.torres@andela.com>
2023-07-20 19:29:31 -0400

    fix: getUrl

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import { useSupabase } from "@/components/SupabaseProvider"
-import { hostUrl } from "@/lib/utils"
 import { Auth } from "@supabase/auth-ui-react"
 import { ThemeSupa } from "@supabase/auth-ui-shared"
+import { useEffect, useRef } from "react"
 // import { signIn } from "next-auth/react"
 
 export default function SignIn() {
@@ -11,13 +11,24 @@ export default function SignIn() {
   // signIn("google", { callbackUrl: "/dashboard" })
 
   const supabase = useSupabase()
+  const host = useRef<string>()
+
+  useEffect(() => {
+    const url = window.location.href
+    host.current = url.substring(
+      0,
+      url.length - window.location.pathname.length
+    )
+    console.log(host.current, url)
+  }, [])
+
   return (
     <div className="flex justify-center height-screen-helper">
       <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
         <Auth
           supabaseClient={supabase}
           providers={["google"]}
-          redirectTo={`${hostUrl()}/auth/callback`}
+          redirectTo={`${host.current}/auth/callback`}
           magicLink={true}
           appearance={{
             theme: ThemeSupa,
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,10 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export const hostUrl = () => {
-  return (
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    "http://localhost:3000"
-  )
-}
