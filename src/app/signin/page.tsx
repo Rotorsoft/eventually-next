eecece2e96af5373e031528a66b1e8b17d1bf939
Roger Torres <roger.torres@andela.com>
2023-07-20 18:35:47 -0400

    fix: getUrl

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,18 +1,10 @@
 "use client"
 
 import { useSupabase } from "@/components/SupabaseProvider"
+import { hostUrl } from "@/lib/utils"
 import { Auth } from "@supabase/auth-ui-react"
 import { ThemeSupa } from "@supabase/auth-ui-shared"
 // import { signIn } from "next-auth/react"
-
-const getURL = () => {
-  console.log("env", JSON.stringify(process.env))
-
-  return (
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    "http://localhost:3000"
-  )
-}
 
 export default function SignIn() {
   // TODO: Next-Auth signin ui
@@ -25,7 +17,7 @@ export default function SignIn() {
         <Auth
           supabaseClient={supabase}
           providers={["google"]}
-          redirectTo={`${getURL()}/auth/callback`}
+          redirectTo={`${hostUrl()}/auth/callback`}
           magicLink={true}
           appearance={{
             theme: ThemeSupa,
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const hostUrl = () => {
+  return (
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000"
+  )
 }
