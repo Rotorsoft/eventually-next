9b04adef9e5b5d7f020aa67a8e4da3936f1da8c4
Roger Torres <roger.torres@andela.com>
2023-07-24 16:46:10 -0400

    fix: add projection

--- src/app/dashboard/bookings/page.tsx
+++ src/app/dashboard/bookings/page.tsx
@@ -10,6 +10,8 @@ import {
 import Totals from "../projectors/Totals"
 import Link from "next/link"
 
+export const dynamic = "force-dynamic"
+
 export default function Bookings() {
   return (
     <div className="flex flex-wrap">
